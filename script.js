f71481d17d6fb32675e78b01042523ea693ed6c2
ko23yun <ko23yun@naver.com>
2021-08-21 09:53:38 +0900

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hi! this is flume");
+const fs = require("fs");
+let input = fs.readFileSync("dev/stdin").toString();
+
+let N = Number(input);
+
+let arr = [0, 1];
+let answer = 1;
+
+for (let i = 1; i < N; i++) {
+  answer = arr[i - 1] + arr[i];
+
+  arr.push(answer);
+}
+
+console.log(arr[N]);
